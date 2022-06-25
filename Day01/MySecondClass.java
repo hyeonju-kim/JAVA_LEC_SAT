369da386b88bbe897a2562a27bf578201d80203b
hyeonju-kim <yocu1784@naver.com>
2022-06-25 16:49:49 +0900

    한줄만추가함

--- Day01/MySecondClass.java
+++ Day01/MySecondClass.java
@@ -7,5 +7,6 @@ public class MySecondClass { // 자바 클래스의 시작
 	public static void main(String[] args ) {
 		//메소드 블록
 		System.out.println("Git Commit후 변경해보기");
+		System.out.println("이것만추가함");
 	}
 } //자바 클래스의 끝
