9ab3ecd07dd2465d2df03f26773dbf55ef4d7e05
hyeonju-kim <yocu1784@naver.com>
2022-06-25 16:20:03 +0900

    나의 첫 깃 커밋

---
+++ Day01/MyFirstClass.java
@@ -0,0 +1,9 @@
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("My First Class"); //My First Class를 출력한다.
+		System.out.println("gkgk"); //gkgk를 출력한다.
+	}
+	
+}
