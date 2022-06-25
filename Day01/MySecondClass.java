2b21ac1546e10cd4f46f160344461e5e71a04e91
hyeonju-kim <yocu1784@naver.com>
2022-06-25 17:23:06 +0900

    집

--- Day01/MySecondClass.java
+++ Day01/MySecondClass.java
@@ -7,6 +7,7 @@ public class MySecondClass { // 자바 클래스의 시작
 	public static void main(String[] args ) {
 		//메소드 블록
 		System.out.println("Git Commit후 변경해보기");
-		System.out.println("이것만추가함");
+		System.out.println("집에서 작업했어요");
+		System.out.println("집에서 작업했어요2");
 	}
 } //자바 클래스의 끝
