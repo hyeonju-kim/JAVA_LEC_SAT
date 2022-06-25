e22cff7342986b54ccb2057555825abde7207757
hyeonju-kim <yocu1784@naver.com>
2022-06-25 16:24:26 +0900

    커밋 테스트

--- Day01/MySecondClass.java
+++ Day01/MySecondClass.java
@@ -1,4 +1,11 @@
 
-public class MySecondClass {
 
-}
+public class MySecondClass { // 자바 클래스의 시작
+	
+	/*클래스 블록*/
+	
+	public static void main(String[] args ) {
+		//메소드 블록
+		System.out.println("Git Commit후 변경해보기");
+	}
+} //자바 클래스의 끝
